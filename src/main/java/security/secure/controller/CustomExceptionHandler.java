eaeaf0065e93ab5e6c3cc2cfca249dbe61eee47a
nico <juannico36@live.com.ar>
2022-04-20 16:19:01 -0300

    cambios menores

--- src/main/java/security/secure/controller/CustomExceptionHandler.java
+++ src/main/java/security/secure/controller/CustomExceptionHandler.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import security.secure.exception.ErrorResponse;
+import security.secure.exception.RecordNotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,4 +29,22 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
         ErrorResponse error = new ErrorResponse("Validation Failed", details);
         return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
     }
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getLocalizedMessage());
+        ErrorResponse error = new ErrorResponse("Server Error", details);
+        return new ResponseEntity(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(RecordNotFoundException.class)
+    public final ResponseEntity<Object> handleUserNotFoundException(RecordNotFoundException ex, WebRequest request) {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getLocalizedMessage());
+        ErrorResponse error = new ErrorResponse("Record Not Found", details);
+        return new ResponseEntity(error, HttpStatus.NOT_FOUND);
+    }
+
+
+
 }
--- src/main/java/security/secure/service/Impl/UserServiceImpl.java
+++ src/main/java/security/secure/service/Impl/UserServiceImpl.java
@@ -83,5 +83,4 @@ public class UserServiceImpl implements UserService , UserDetailsService {
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         return findByUsername(s);
     }
-    //mensaje de ejemplo
 }
